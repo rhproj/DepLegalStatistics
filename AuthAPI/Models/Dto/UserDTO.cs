a5360da6f4236d58a45ceb6ebe1b76f738fd6607
Rh Proj <r.proj@hotmail.com>
2024-02-09 11:15:45 +0300

    AuthAPI : AuthService

--- AuthAPI/Models/Dto/UserDTO.cs
+++ AuthAPI/Models/Dto/UserDTO.cs
@@ -4,5 +4,6 @@
 	{
 		public string ID { get; set; }
 		public string Email { get; set; }
-	} 
+        public string PhoneNumber { get; set; }
+    } 
 }
--- AuthAPI/Service/AuthService.cs
+++ AuthAPI/Service/AuthService.cs
@@ -1,23 +1,102 @@
-﻿using AuthAPI.Models.Dto;
+﻿using AuthAPI.Data;
+using AuthAPI.Models.Dto;
 using AuthAPI.Service.IService;
+using Microsoft.AspNetCore.Identity;
 
 namespace AuthAPI.Service
 {
     public class AuthService : IAuthService
     {
-        public Task<bool> AssignRole(string email, string roleName)
+        private readonly AppDbContext _dbContext;  //Эти объекты идут из ASP.Identity и помогут с рег-ей пользователя,хэшир-ем паролей, Входом и т.п
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AuthService(AppDbContext db, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager) //, IJwtTokenGenerator jwtTokenGenerator)
         {
-            throw new NotImplementedException();
+            _dbContext = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            //_jwtTokenGenerator = jwtTokenGenerator;
         }
 
-        public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
-        {
-            throw new NotImplementedException();
+        public async Task<string> Register(RegistrationRequestDto registrationRequestDto)
+        { //создадим нового пользователя
+            IdentityUser user = new()
+            {
+                UserName = registrationRequestDto.Email,
+                Email = registrationRequestDto.Email,
+                NormalizedEmail = registrationRequestDto.Email.ToUpper(),
+                PhoneNumber = registrationRequestDto.PhoneNumber
+            };
+
+            try
+            {
+                var result = await _userManager.CreateAsync(user, registrationRequestDto.Password);
+                if (result.Succeeded) 
+                {
+                    var userToReturn = _dbContext.IdentityUser.First(u => u.UserName == registrationRequestDto.Email);
+                    UserDTO userDto = new()
+                    {
+                        Email = userToReturn.Email,
+                        ID = userToReturn.Id,
+                        PhoneNumber = userToReturn.PhoneNumber
+                    };
+                    return string.Empty;
+                }
+                else
+                {
+                    return result.Errors.FirstOrDefault().Description;
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return "Обнаружена ошибка";
         }
 
-        public Task<string> Register(RegistrationRequestDto registrationRequestDto)
+        public async Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
         {
-            throw new NotImplementedException();
+            IdentityUser? user = _dbContext.IdentityUser.FirstOrDefault(u => u.UserName.ToLower() == loginRequestDto.UserName.ToLower());
+            bool isValid = await _userManager.CheckPasswordAsync(user, loginRequestDto.Password);
+
+            if (user == null || isValid == false)
+            {
+                return new LoginResponseDto() { User = null, Token = string.Empty };
+            }
+            var roles = await _userManager.GetRolesAsync(user);
+            //var token = _jwtTokenGenerator.GenerateToken(user, roles);
+
+            UserDTO userDTO = new()
+            {
+                Email = user.Email,
+                ID = user.Id,
+                PhoneNumber = user.PhoneNumber
+            };
+
+            LoginResponseDto loginResponseDto = new LoginResponseDto()
+            {
+                User = userDTO,
+                Token = string.Empty //token
+            };
+            return loginResponseDto;
         }
+
+        public async Task<bool> AssignRole(string email, string roleName)
+        {
+            var user = _dbContext.IdentityUser.FirstOrDefault(u => u.Email.ToLower() == email.ToLower());
+            if (user != null)
+            {
+                if (!_roleManager.RoleExistsAsync(roleName).GetAwaiter().GetResult())
+                {
+                    _roleManager.CreateAsync(new IdentityRole(roleName)).GetAwaiter().GetResult();
+                }
+                await _userManager.AddToRoleAsync(user, roleName);
+                return true;
+            }
+            return false;
+        }
+
+
+
     }
 }
